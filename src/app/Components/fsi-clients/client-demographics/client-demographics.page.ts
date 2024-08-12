f8c11fff1b4681be8153181d13639e3d7a63f102
Your Name <you@example.com>
2024-08-12 14:18:19 -0500

    demographics are displayed and able to be editted

--- src/app/Components/fsi-clients/client-demographics/client-demographics.page.ts
+++ src/app/Components/fsi-clients/client-demographics/client-demographics.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Demographics } from 'src/app/Models/demographics';
 import { DemographicsService } from 'src/app/Services/demographics.service';
 
@@ -11,11 +11,34 @@ import { DemographicsService } from 'src/app/Services/demographics.service';
 export class ClientDemographicsPage implements OnInit {
   userName: string = '';
   isVisible: boolean = false;
+  isFormVisible = false;
 
   userDemographics: Demographics[] = [];
+  demographic: Demographics = {
+    demographicsId: 0,
+    spouse: '',
+    c1: '',
+    c2: '',
+    c3: '',
+    c4: '',
+    socialSecurity: '',
+    driversLicense: '',
+    issueDate: '',
+    expirationDate: '',
+    gender: '',
+    maritalStatus: '',
+    employer: '',
+    occupation: '',
+    workPhone: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+    birthday: '',
+    username: this.actRouter.snapshot.paramMap.get("username") ?? ''
+  }
   none = false;
   input = false;
-  constructor(private demoService: DemographicsService, private actRouter: ActivatedRoute) { }
+  constructor(private demoService: DemographicsService, private actRouter: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     const name = this.actRouter.snapshot.paramMap.get("username") ?? '';
@@ -35,8 +58,26 @@ export class ClientDemographicsPage implements OnInit {
       }
     })
   }
+  createDemographic(){
+    this.demoService.CreateDemographic(this.demographic).subscribe(() => {
+
+    }, error => {
+      console.log('Error: ', error)
+      if(error.status == 401){
+        this.router.navigate(['/login'])
+      }
+    })
+    window.location.reload();
+  }
   showElement(): void {
     this.isVisible = !this.isVisible;
+  }
+  openForm(){
+    this.isFormVisible = true;
+  }
+
+  closeForm(){
+    this.isFormVisible = false;
   }
 
 
--- src/app/Components/fsi-clients/client-edit-demo/client-edit-demo.page.ts
+++ src/app/Components/fsi-clients/client-edit-demo/client-edit-demo.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Demographics } from 'src/app/Models/demographics';
+import { DemographicsService } from 'src/app/Services/demographics.service';
 
 @Component({
   selector: 'app-client-edit-demo',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-edit-demo.page.scss'],
 })
 export class ClientEditDemoPage implements OnInit {
+  id: string = "0";
 
-  constructor() { }
+  currentDemographics: Demographics = {
+    demographicsId: 0,
+    spouse: '',
+    c1: '',
+    c2: '',
+    c3: '',
+    c4: '',
+    socialSecurity: '',
+    driversLicense: '',
+    issueDate: '',
+    expirationDate: '',
+    gender: '',
+    maritalStatus: '',
+    employer: '',
+    occupation: '',
+    workPhone: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+    birthday: '',
+    username: this.actRouter.snapshot.paramMap.get("username") ?? ''
+  }
+
+  constructor(private actRouter: ActivatedRoute, private myDemoService: DemographicsService, private router: Router) { }
 
   ngOnInit() {
+    const routeId = this.actRouter.snapshot.paramMap.get("id") ?? "";
+    this.id = routeId;
+    this.myDemoService.GetDemographicsById(parseInt(this.id)).subscribe(foundDemo => {
+      this.currentDemographics = foundDemo;
+      console.log(foundDemo)
+    })
+  }
+
+  onSubmit(){
+    this.myDemoService.UpdateDemographic(parseInt(this.id), this.currentDemographics).subscribe(() => {
+      window.alert("Demographics updated successfully")
+      this.router.navigate(['/demographics', this.currentDemographics.username])
+    }, error => {
+      console.log("Error: ", error)
+      if(error.status == 401){
+        this.router.navigate(['/login'])
+      }
+    })
   }
 
 }

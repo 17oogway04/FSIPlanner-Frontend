2c64970a753db9dd733c5e62b96c1913fe7e4ba3
Your Name <you@example.com>
2024-08-12 10:39:11 -0500

    demographic page updated and got search working for users

---
+++ src/app/Components/search-users/search-users.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/Models/user';
+import { UserService } from 'src/app/Services/user.service';
+
+@Component({
+  selector: 'app-search-users',
+  templateUrl: './search-users.page.html',
+  styleUrls: ['./search-users.page.scss'],
+})
+export class SearchUsersPage implements OnInit {
+  searchStr!: string;
+  users: User = {
+    userId: "0",
+    firstName: '',
+    lastName: '',
+    userName: '',
+    password: '',
+    profilePicture: ''
+  }
+  constructor(private myUserService: UserService) { }
+
+  ngOnInit() {
+ 
+  }
+
+  searchUser(){
+    this.searchUserByName(this.searchStr)
+  }
+
+  async searchUserByName(username: string){
+    this.myUserService.getUserByName(username).subscribe((response => {
+      this.users.firstName = response.firstName;
+      this.users.lastName = response.lastName;
+      this.users.userName = response.userName;
+    }))
+  }
+
+}

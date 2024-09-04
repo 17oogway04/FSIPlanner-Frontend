2e6646e444690f0078b0a04a45745c1b9263ec6f
Your Name <you@example.com>
2024-09-04 10:35:59 -0700

    minor fixes

--- src/app/Components/home/home.page.ts
+++ src/app/Components/home/home.page.ts
@@ -13,6 +13,7 @@ export class HomePage implements OnInit {
   baseUrl: string = "http://localhost:5247/api/user"
   isAuthenticated: boolean = false;
   userName: string = '';
+  isAllowed: boolean = true;
   presentUser: User ={
     firstName: '',
     lastName: '',
@@ -24,14 +25,14 @@ export class HomePage implements OnInit {
   file: string = '';
   constructor(private myUserservice: UserService, private actRouter: ActivatedRoute, private http: HttpClient) { }
 
-  ngOnInit() {
+  ngOnInit() {      
     this.myUserservice.getCurrentUser().subscribe(response => {
       this.loggedInUser = this.presentUser = response;
       this.userName = this.presentUser.userName ?? '';
       this.myUserservice.isLoggedInSubj.next(!response.userId)
-      const name = this.actRouter.snapshot.paramMap.get("username") ?? ''
+      const name = this.actRouter.snapshot.paramMap.get("username") ?? ''    
       if(name !== ''){
-        this.userName = name;
+        this.userName = name;        
         this.myUserservice.getUserByUsername(name).subscribe(response => {
           this.presentUser = response;
         }) 
@@ -42,13 +43,20 @@ export class HomePage implements OnInit {
         this.isAuthenticated = isLoggedIn;
         this.isAuthenticated = isLoggedIn;
       })
-
     }
     logout(){
       this.myUserservice.logout()
     }
-  }
 
+    checkUsername(){
+      if(this.userName === "isaacm@mutualmail.com" || "jenniferh@mutualmail.com"){
+        this.isAllowed = true;
+      }else{
+        this.isAllowed = false;
+      }
+    }
+ 
+}
 //   onFileChange(event: any) {
 //     const files = event.target.files as FileList;
 
